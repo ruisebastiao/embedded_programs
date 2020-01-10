30e242fa7c5e8c3eb374b7a90574766364fc2613
Rui Sebastiao <ruisebastiao@gmail.com>
2020-01-10 15:41:11 +0000

    updated

--- java/JavaFXFXML/src/javafxfxml/JavaFXFXML.java
+++ java/JavaFXFXML/src/javafxfxml/JavaFXFXML.java
@@ -5,25 +5,101 @@
  */
 package javafxfxml;
 
-import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.PointLight;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.stage.Stage;
+ 
 
 /**
  *
  * @author rui
  */
 public class JavaFXFXML extends Application {
+      double anchorX, anchorY, anchorAngle;
+ 
+    private PerspectiveCamera addCamera(Scene scene) {
+        PerspectiveCamera perspectiveCamera = new PerspectiveCamera(false);
+        scene.setCamera(perspectiveCamera);
+        return perspectiveCamera;
+    }
     
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root=FXMLLoader.load(getClass().getResource("mainFXML.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("Teste Title");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        
+
+        primaryStage.setTitle("SphereAndBox");
+ 
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setSpecularColor(Color.ORANGE);
+        redMaterial.setDiffuseColor(Color.RED);
+ 
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.BLUE);
+        blueMaterial.setSpecularColor(Color.LIGHTBLUE);
+ 
+        final Box red = new Box(400, 400, 400);
+        red.setMaterial(redMaterial);
+ 
+        final Sphere blue = new Sphere(200);
+        blue.setMaterial(blueMaterial);
+ 
+        blue.setTranslateX(250);
+        blue.setTranslateY(250);
+        blue.setTranslateZ(50);
+        red.setTranslateX(250);
+        red.setTranslateY(250);
+        red.setTranslateZ(450);
+ 
+        final Group parent = new Group(red, blue);
+        parent.setTranslateZ(500);
+        parent.setRotationAxis(Rotate.Y_AXIS);
+ 
+ 
+        final Group root = new Group(parent);
+ 
+        final Scene scene = new Scene(root, 480, 320, true);
+ 
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event) {
+                anchorX = event.getSceneX();
+                anchorY = event.getSceneY();
+                anchorAngle = parent.getRotate();
+            }
+        });
+ 
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event) {
+                parent.setRotate(anchorAngle + anchorX -  event.getSceneX());
+            }
+        });
+ 
+        PointLight pointLight = new PointLight(Color.ANTIQUEWHITE);
+        pointLight.setTranslateX(15);
+        pointLight.setTranslateY(-10);
+        pointLight.setTranslateZ(-100);
+ 
+        root.getChildren().add(pointLight);
+ 
+        addCamera(scene);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
     }
     
     
